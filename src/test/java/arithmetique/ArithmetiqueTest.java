018b7424e9f54a25663a6ad10f826c000ab3558d
Emmanuel MOUPOJOU <moupojouemma@yahoo.fr>
2019-04-24 18:04:46 +0200

    @Test on test method added

--- src/test/java/arithmetique/ArithmetiqueTest.java
+++ src/test/java/arithmetique/ArithmetiqueTest.java
@@ -1,9 +1,11 @@
 package arithmetique;
 
 import org.junit.Assert;
+import org.junit.Test;
 
 public class ArithmetiqueTest {
 
+    @Test
     public void additionTest(){
         Assert.assertTrue(2==(1+1));
     }
